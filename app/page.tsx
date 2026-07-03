a25b3037897c4ad677acc54a47dbf1253d0f4b84
Mohammed-Ashik-T <ashikchamp01@gmail.com>
2026-07-03 17:27:27 +0530

    COrrect Loading

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,12 @@ import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
-import LoadingScreen from "@/components/LoadingScreen";
 import Footer from "@/components/Footer";
 
 
 export default function Home() {
   return (
     <>
-      <LoadingScreen />
-
       <Navbar />
 
       <main>
